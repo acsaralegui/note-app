ab062c41c7680edd9c0d0356c0a01f2019ccc83e
acsaralegui <aaron.saralegui@gmail.com>
2019-08-19 03:36:43 +0000

    finish the Nav Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 mport React, { Component } from 'react';
 import './App.css';
 import Nav from './components/Nav';
+import List from './componenets/List';
+import Note from './components/Note';
 
 class App extends Component {
 render() {
   return (
     <div className="App">
-      <div>
-        Nav Component
-      </div>
+      <Nav /> 
+      <List />
+      <Note />
     </div>
   );
 }
---
+++ src/components/List.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class List extends Ract.Component {
+  render(){
+    return(
+      <div className="list-container")>
+        List Component
+      </div>
+        );
+  }
+}
+
+export default List;
