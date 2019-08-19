524dcd1d312ad66312ea0a290c896ec6b60cefb5
acsaralegui <aaron.saralegui@gmail.com>
2019-08-19 05:08:43 +0000

    toggling ability

--- src/components/List.js
+++ src/components/List.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-class List extends Ract.Component {
+class List extends React.Component {
   render(){
     return(
-      <div className="list-container")>
+      <div className="list-container">
         List Component
       </div>
         );
