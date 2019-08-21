35a7918f23dbf6ad0dcacf6895a9cddbbf92ee55
acsaralegui <aaron.saralegui@gmail.com>
2019-08-21 03:23:36 +0000

    fixed note-API linked

--- src/helpers/urlFor.js
+++ src/helpers/urlFor.js
@@ -1,5 +1,5 @@
 const urlFor = (endpoint) => {
-  return 'https://note-api-saralegui.herokuapp.com/notes' + endpoint;
+  return 'https://note-api-saralegui.herokuapp.com/' + endpoint;
 };
 
 export default urlFor;
